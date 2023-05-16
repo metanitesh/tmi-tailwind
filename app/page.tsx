f701971d20131d2060f3a7a62e37b26af311f5ab
nitesh sharma <nitesh.sharma@deliveryhero.com>
2023-05-16 08:07:51 +0200

    Update copy for landing page

--- app/page.tsx
+++ app/page.tsx
@@ -73,14 +73,17 @@ export default function LandingPage() {
           <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
             <div className="lg:pr-8 lg:pt-4">
               <div className="lg:max-w-lg">
-                <h2 className="text-base font-light leading-7 text-orange-700 underline">
+                <h2 className="text-base font-light leading-7 text-orange-700">
                   Based on{" "}
                   <span className="font-bold ">The mind illuminated </span>
-                  and Samatha tradition
                 </h2>
-                <p className="mt-2 text-xl font-bold tracking-tight text-gray-900 sm:text-2xl text-orange-700">
-                  Cultivate strong meditation practice through ten progressive
-                  stages
+                <p className="mt-2 text-xl font-light tracking-tight text-gray-900 sm:text-2xl text-orange-700">
+                  Cultivate{" "}
+                  <span className="font-bold">
+                    stable and focused attention
+                  </span>{" "}
+                  through ten progressive stages of
+                  <span className="font-bold"> Samatha meditation</span>
                 </p>
 
                 <p className="mt-6 text-lg leading-8 text-gray-600">
@@ -115,7 +118,7 @@ export default function LandingPage() {
             <img
               src="/samatha.jpeg"
               alt="Product screenshot"
-              className="w-[10rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[42rem] md:-ml-4 lg:-ml-0"
+              className="w-[30rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[42rem] md:-ml-4 lg:-ml-0"
               width={600}
               height={1000}
             />
